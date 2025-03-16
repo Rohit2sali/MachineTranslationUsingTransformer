8733f950bfd02800f8922ea03d2faef98662d75b
Rohit Sali <155815075+Rohit2sali@users.noreply.github.com>
2025-03-16 14:19:06 +0530

    Update decoder.py

--- decoder.py
+++ decoder.py
@@ -2,7 +2,7 @@ from Attention import MultiHeadAttention
 from LayerNorm import LayerNorm
 import torch.nn as nn
 
-class Decoder(nn.Module):
+class Decoder(nn.Module): 
     def __init__(self, d_model, n_heads, fnn_hidden_dim, eps):
         super(Decoder, self).__init__()
         self.d_model = d_model
@@ -14,19 +14,20 @@ class Decoder(nn.Module):
         self.fnn = nn.Sequential(
             nn.Linear(d_model, fnn_hidden_dim),
             nn.ReLU(),
+            nn.Dropout(0.1),
             nn.Linear(fnn_hidden_dim, d_model)
         )
 
         self.layernorm1 = LayerNorm(d_model, eps)
         self.layernorm2 = LayerNorm(d_model, eps)
         self.layernorm3 = LayerNorm(d_model, eps)
-
-    def forward(self, embedding_output, query, padding_mask, lookahead_mask):
-        maskedattention_output = self.maskedattention.forward(embedding_output, embedding_output, embedding_output, padding_mask, lookahead_mask, self.n_heads, masked=True)
+        
+    def forward(self, embedding_output, query, decoder_padding_mask, encoder_padding_mask, lookahead_mask):
+        maskedattention_output = self.maskedattention.forward(embedding_output, embedding_output, embedding_output, decoder_padding_mask, lookahead_mask=lookahead_mask, n_heads=self.n_heads)
         input = maskedattention_output + embedding_output
         value = self.layernorm1.forward(input)
-
-        attention_output = self.cross_attention.forward(query, query, value, padding_mask, lookahead_mask, self.n_heads, masked=False)
+ 
+        attention_output = self.cross_attention.forward(query, query, value, encoder_padding_mask, lookahead_mask=None, n_heads=self.n_heads)
         attention_output = attention_output + value
         attention_output = self.layernorm2.forward(attention_output)
 
@@ -34,4 +35,4 @@ class Decoder(nn.Module):
         output = output + attention_output
         output = self.layernorm3.forward(output)
 
-        return output
+        return output  
