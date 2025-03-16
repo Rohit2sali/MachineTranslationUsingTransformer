6ca49cf9c9eb676ea4235120c71cc5390275e6ef
Rohit Sali <155815075+Rohit2sali@users.noreply.github.com>
2025-03-16 14:24:43 +0530

    Update train.py

--- train.py
+++ train.py
@@ -3,26 +3,26 @@ from get_data import data
 import torch
 import torch.nn as nn
 
+import matplotlib.pyplot as plt
+scaler = torch.cuda.amp.GradScaler()
+
 def generate_text(predictions):
     predictions = predictions.permute(0, 2, 1)
-    
     predictions = torch.nn.functional.softmax(predictions, dim=-1)
-   
-    _, predictions = torch.max(predictions, dim=-1)
-    output = model.decode(predictions)
+    _, predictions = torch.max(predictions, dim=-1) 
+    output = tokenizer.decode(predictions, mode="test")
     print(output[0])
-
-
-def accuracy_fn(target, prediction, eos_token):
+    print(output[100])
+    print(output[250])
+    
+def accuracy_fn(prediction, target, eos_token):   
     prediction = prediction.permute(0, 2, 1)
     prediction = torch.nn.functional.softmax(prediction, dim=-1)
-    prediction = torch.max(prediction, dim=-1)
-    prediction = prediction[1]
-    m = len(prediction[1])
+    _, prediction = torch.max(prediction, dim=-1)
+    m = 0
     for i in range(len(target)):
         for j in range(len(target[0])):
-            if(target[i][j] == eos_token):
-                if(j>m):
+            if(target[i][j] == eos_token and j > m):
                     m = j
                     break
 
@@ -34,84 +34,123 @@ def accuracy_fn(target, prediction, eos_token):
 
 def calculate_loss(prediction, target_tokens, pading_token):
     criterion = nn.CrossEntropyLoss(ignore_index=pading_token)
-    return criterion(prediction, target_tokens)
+    loss = criterion(prediction, target_tokens)
+    return loss
 
-def train(input, target, val_input, val_target):
-    train_loss = []
-    train_acc = []
-    val_loss = []
-    val_acc = []
-
-    model.train()
-    prediction, target_tokens, eos_token, padding_token = model(input, target, generate=False)
-    loss = calculate_loss(prediction, target_tokens, padding_token)
-    accuracy = accuracy_fn(target_tokens, prediction, eos_token)
-    train_loss.append(loss)
-    train_acc.append(accuracy)
-    optimizer.zero_grad()
-    loss.backward()
-    optimizer.step()
+def eval(val_input_tokens, val_target_tokens):
+    model.eval()    
     
-    model.eval()
     with torch.no_grad():
-        prediction, target_tokens, eos_token, padding_token = model(val_input, val_target, generate=False)
-        valid_loss = calculate_loss(prediction, target_tokens, padding_token)
-        val_accuracy = accuracy_fn(target_tokens, prediction, eos_token)
-        val_loss.append(valid_loss)
-        val_acc.append(val_accuracy)
-        
-    train_loss = sum(train_loss) / len(train_loss)
-    train_acc = sum(train_acc) / len(train_acc)
-    val_loss = sum(val_loss) / len(val_loss)
-    val_acc = sum(val_acc) / len(val_acc)
-    return train_loss, train_acc, val_loss, val_acc
+        prediction, eos_token, padding_token = model(val_input_tokens, val_target_tokens)
+        loss = calculate_loss(prediction, val_target_tokens, padding_token)
+        accuracy = accuracy_fn(prediction, val_target_tokens, eos_token)
+    return prediction, loss, accuracy
+
+def train(input_tokens, target_tokens):
+    model.train()
+    with torch.cuda.amp.autocast():
+        prediction, eos_token, padding_token = model(input_tokens, target_tokens)
+        loss = calculate_loss(prediction, target_tokens, padding_token)
+        accuracy = accuracy_fn(prediction, target_tokens, eos_token)
+    optimizer.zero_grad()
+    scaler.scale(loss).backward()  # Scale loss to prevent underflow
+    scaler.step(optimizer)  # Update model weights
+    scaler.update()
+    return loss, accuracy
 
 def test(test_data, target_test_data):
-    # test_data = test_data[:batch_size]
-    # target_test_data = target_test_data[:batch_size]
     with torch.no_grad():
-        prediction, target_tokens, eos_token, padding_token = model(test_data, target_test_data, generate=False)
-        test_loss = calculate_loss(prediction, target_tokens, padding_token)
-        test_acc = accuracy_fn(target_tokens, prediction, eos_token)
+        prediction, eos_token, padding_token = model(test_data, target_test_data)
+        
+        test_loss = calculate_loss(prediction, target_test_data, padding_token)
+        test_acc = accuracy_fn(prediction, target_test_data, eos_token)
         print("test_loss :",test_loss, "test_acc :", test_acc)
-    return prediction, eos_token
-
+    return prediction
 
 if __name__ == "__main__":
-    max_seq_len = 10
+    max_seq_len = 64
     n_heads = 8
-    d_model = 128
-    fnn_hidden_dim = 2048
-    n_layers = 2
+    d_model = 256
+    fnn_hidden_dim = 2024
+    n_layers = 4
     eps = 1e-9
-    n_epoch = 5
-    batch_size = 5
+    n_epoch = 10
+    batch_size = 32
 
-    a, b, train_data, val_data, test_data, target_train_data, target_val_data, target_test_data = data().get_data(4000)
+    with open("/kaggle/input/datasetfortranslation/vocab.pkl", "rb") as f:
+        vocab = pickle.load(f)
 
-    model = Transformer(a, b, max_seq_len, d_model, n_heads, n_layers, fnn_hidden_dim, eps)
+    train_data, val_data, test_data, target_train_data, target_val_data, target_test_data = data().get_data()
+    
+    tokenizer = Tokenization(vocab, max_seq_len)
+    train_data = tokenizer.tokenize(train_data, generate=False)
+    val_data = tokenizer.tokenize(val_data, generate=False)
+    test_data = tokenizer.tokenize(test_data, generate=False)
+    target_train_data = tokenizer.tokenize(target_train_data, generate=False)
+    target_val_data = tokenizer.tokenize(target_val_data, generate=False)
+    target_test_data = tokenizer.tokenize(target_test_data, generate=False)
+    
+    
+    model = Transformer(vocab, max_seq_len, d_model, n_heads, n_layers, fnn_hidden_dim, eps)
 
-    optimizer = torch.optim.AdamW(model.parameters(), lr=1e-3)
+    optimizer = torch.optim.AdamW(model.parameters(), lr=1e-3, weight_decay=1e-2)
 
-    # criterion = nn.CrossEntropyLoss(ignore_index=pad_token)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = model.to(device)
 
     best_val_loss = float("inf")
     n_batches = len(train_data) // batch_size
+    train_losses = []
+    train_accuracies = []
+    val_losses = []
+    val_accuracies = []
     for epoch in range(n_epoch):
+        train_loss = 0
+        train_acc = 0
         for i in range(0, len(train_data), batch_size):
-            input = train_data[0:batch_size]
-            target = val_data[0:batch_size]
-            val_input = val_data[0:batch_size]
-            val_target = target_val_data[0:batch_size]
-            train_loss, train_acc, val_loss, val_acc = train(input, target, val_input, val_target)
+            input_tokens = train_data[i:i+batch_size].to(device)
+            target_tokens = target_train_data[i:i+batch_size].to(device)
+            loss, acc= train(input_tokens, target_tokens)
+            train_loss += loss.item() 
+            train_acc += acc
+            if(i % 1000 == 0):
+                print("at this i :",i)
 
+        train_loss = train_loss / (len(train_data) / batch_size)
+        train_acc = train_acc/(len(train_data) / batch_size)
+        train_losses.append(train_loss)
+        train_accuracies.append(train_acc)
+        predictions, val_loss , val_acc = eval(val_data.to(device), target_val_data.to(device))
+        val_losses.append(val_loss.item())
+        val_accuracies.append(val_acc)
+        print(f"at epoch : {epoch}, train_loss : {train_loss}, train_acc : {train_acc}")
+        print(f"at epoch : {epoch}, val_loss : {val_loss}, val_acc : {val_acc}")
+        generate_text(predictions)
+        torch.cuda.empty_cache()
+        if (epoch == 1):
             if val_loss < best_val_loss:
                 best_val_loss = val_loss
                 torch.save(model.state_dict(), f'transformer_epoch{epoch + 1}.pth')
-        print(f"at epoch :{epoch}, loss :{train_loss}, acc :{train_acc}, val_loss{val_loss}, val_acc :{val_acc}")
-           
-        predictions, eos_token = test(test_data, target_test_data)
-        generate_text(predictions)
-    torch.save(model.state_dict(), 'model.pth')
+    predictions = test(test_data.to(device), target_test_data.to(device))
+    generate_text(predictions)
+    torch.save(model, 'model.pth')
+    plt.figure(figsize=(10, 4))
+    plt.subplot(1, 2, 1)
+    plt.plot(range(1, n_epoch + 1), train_losses, label="Traing Loss", marker='o')
+    plt.plot(range(1, n_epoch + 1), val_losses, label="Validation Loss", marker='o')
+    plt.xlabel("Epoch")
+    plt.ylabel("Loss")
+    plt.title("Loss Graph")
+    plt.legend()
+    plt.grid()
+    plt.show()
 
-
+    plt.subplot(1, 2, 1)
+    plt.plot(range(1, n_epoch + 1), train_accuracies, label="Traning Accu", marker='o')
+    plt.plot(range(1, n_epoch + 1), val_accuracies, label="Validation Accu", marker='o')
+    plt.xlabel("Epoch")
+    plt.ylabel("Accuracy")
+    plt.title("Accuracy Graph")
+    plt.legend()
+    plt.grid()
+    plt.show()
