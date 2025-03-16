3fedb5c8d3cd91842a75533773a6b629ad700c36
Rohit Sali <155815075+Rohit2sali@users.noreply.github.com>
2025-03-16 14:13:49 +0530

    Update Attention.py

--- Attention.py
+++ Attention.py
@@ -9,7 +9,7 @@ class MultiHeadAttention(nn.Module):
         self.w_v = torch.nn.Linear(d_model, d_model)
         self.w_out = torch.nn.Linear(d_model, d_model)
     
-    def forward(self, query, key, value, padding_mask, lookahead_mask, n_heads, masked = bool):
+    def forward(self, query, key, value, padding_mask, lookahead_mask, n_heads):
         batch_size, max_seq_len, d_model = query.shape
         q = self.w_q(query)
         k = self.w_k(key)
@@ -19,16 +19,17 @@ class MultiHeadAttention(nn.Module):
         q = q.view(batch_size, max_seq_len, n_heads, d_k).permute(0, 2, 1, 3)
         k = k.view(batch_size, max_seq_len, n_heads, d_k).permute(0, 2, 1, 3)
         v = v.view(batch_size, max_seq_len, n_heads, d_k).permute(0, 2, 1, 3)
-
-        attention_scores = torch.matmul(q, k.transpose(-2, -1))/torch.sqrt(torch.tensor(d_k, dtype=torch.float32)) # (-2, -1) will swap the last two dimension of the matrix
-        if(masked == True):
+    
+        attention_scores = torch.matmul(q, k.transpose(-2, -1))/ np.sqrt(d_model) # (-2, -1) will swap the last two dimension of the matrix
+        padding_mask = padding_mask.to(attention_scores.device)
+        if(lookahead_mask is not None):
+            lookahead_mask = lookahead_mask.to(attention_scores.device)
             attention_scores = attention_scores + padding_mask + lookahead_mask
         else:
             attention_scores = attention_scores + padding_mask
 
         attention_weights = torch.nn.functional.softmax(attention_scores, dim=-1)
         attention = torch.matmul(attention_weights, v)
-
         attention_output = attention.permute(0, 2, 1, 3).contiguous()
         attention_output = attention_output.view(batch_size, max_seq_len, d_model)
         output = self.w_out(attention_output)
