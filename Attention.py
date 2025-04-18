2840bd839f46d0ed78d2d914f3c8cbe021e7483e
Rohit Sali <155815075+Rohit2sali@users.noreply.github.com>
2025-04-18 14:07:11 +0530

    Update Attention.py

--- Attention.py
+++ Attention.py
@@ -1,6 +1,6 @@
 import torch
+import numpy as np
 import torch.nn as nn
-
 class MultiHeadAttention(nn.Module):
     def __init__(self, d_model): # tokens should be of shape (batch_size, max_seq_len), the one before embedding
         super(MultiHeadAttention, self).__init__()
@@ -19,9 +19,10 @@ class MultiHeadAttention(nn.Module):
         q = q.view(batch_size, max_seq_len, n_heads, d_k).permute(0, 2, 1, 3)
         k = k.view(batch_size, max_seq_len, n_heads, d_k).permute(0, 2, 1, 3)
         v = v.view(batch_size, max_seq_len, n_heads, d_k).permute(0, 2, 1, 3)
-    
-        attention_scores = torch.matmul(q, k.transpose(-2, -1))/ np.sqrt(d_model) # (-2, -1) will swap the last two dimension of the matrix
+       
+        attention_scores = torch.matmul(q, k.transpose(-2, -1))/ np.sqrt(d_k) # (-2, -1) will swap the last two dimension of the matrix
         padding_mask = padding_mask.to(attention_scores.device)
+        
         if(lookahead_mask is not None):
             lookahead_mask = lookahead_mask.to(attention_scores.device)
             attention_scores = attention_scores + padding_mask + lookahead_mask
