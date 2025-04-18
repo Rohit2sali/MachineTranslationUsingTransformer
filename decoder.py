730a617fb8b2c52e331e9e1bc927f7bbd0c078cf
Rohit Sali <155815075+Rohit2sali@users.noreply.github.com>
2025-04-18 14:08:45 +0530

    Update decoder.py

--- decoder.py
+++ decoder.py
@@ -1,9 +1,8 @@
-from Attention import MultiHeadAttention
-from LayerNorm import LayerNorm
 import torch.nn as nn
+from Attention import MultiHeadAttention
 
 class Decoder(nn.Module): 
-    def __init__(self, d_model, n_heads, fnn_hidden_dim, eps):
+    def __init__(self, d_model, n_heads, fnn_hidden_dim):
         super(Decoder, self).__init__()
         self.d_model = d_model
         self.n_heads = n_heads
@@ -18,21 +17,20 @@ class Decoder(nn.Module):
             nn.Linear(fnn_hidden_dim, d_model)
         )
 
-        self.layernorm1 = LayerNorm(d_model, eps)
-        self.layernorm2 = LayerNorm(d_model, eps)
-        self.layernorm3 = LayerNorm(d_model, eps)
+        self.layernorm1 = nn.LayerNorm(d_model)
+        self.layernorm2 = nn.LayerNorm(d_model)
+        self.layernorm3 = nn.LayerNorm(d_model)
+
+    def forward(self, embedding_output, key, decoder_padding_mask, encoder_padding_mask, lookahead_mask):
+        x = self.layernorm1(embedding_output)
+        maskedattention_output = self.maskedattention(x, x, x, decoder_padding_mask, lookahead_mask=lookahead_mask, n_heads=self.n_heads)
+        attention_output = maskedattention_output + embedding_output
         
-    def forward(self, embedding_output, query, decoder_padding_mask, encoder_padding_mask, lookahead_mask):
-        maskedattention_output = self.maskedattention.forward(embedding_output, embedding_output, embedding_output, decoder_padding_mask, lookahead_mask=lookahead_mask, n_heads=self.n_heads)
-        input = maskedattention_output + embedding_output
-        value = self.layernorm1.forward(input)
- 
-        attention_output = self.cross_attention.forward(query, query, value, encoder_padding_mask, lookahead_mask=None, n_heads=self.n_heads)
-        attention_output = attention_output + value
-        attention_output = self.layernorm2.forward(attention_output)
+        query = self.layernorm2(attention_output)
+        cross_attention_output = self.cross_attention(query, key, key, encoder_padding_mask, lookahead_mask=None, n_heads=self.n_heads)
+        attention_output = cross_attention_output + attention_output
 
-        output = self.fnn(attention_output)
+        output = self.fnn(self.layernorm3(attention_output))
         output = output + attention_output
-        output = self.layernorm3.forward(output)
 
-        return output  
+        return output   
