aeb6ff15acad5ddc2246b558bcd0cbf0a06feb80
Rohit Sali <155815075+Rohit2sali@users.noreply.github.com>
2025-04-18 14:13:08 +0530

    Update get_data.py

--- get_data.py
+++ get_data.py
@@ -1,19 +1,20 @@
-import pickle
-import pickle
-class data:
-    def __init__(self):
-        with open(r"/all_data/final_train_data.pkl", "rb") as f:
-            self.train_data = pickle.load(f)
-        with open(r"/all_data/test_data.pkl", "rb") as f:
-            self.test_data = pickle.load(f)
-        with open(r"/all_data/val_data.pkl", "rb") as f:
-            self.val_data = pickle.load(f)
-        with open(r"/all_data/final_target_train_data.pkl", "rb") as f:
-            self.target_train_data = pickle.load(f)
-        with open(r"/all_data/target_test_data.pkl", "rb") as f:
-            self.target_test_data = pickle.load(f)
-        with open(r"/all_data/target_val_data.pkl", "rb") as f:
-            self.target_val_data = pickle.load(f)
+import torch
+english = "path\english"
+french = "path\french"
 
-    def get_data(self):
-        return self.train_data, self.val_data, self.test_data, self.target_train_data, self.target_val_data, self.target_test_data
+class get_data:
+    def __init__(self, max_seq_len):
+        tokenizer = Tokenization(max_seq_len)
+        train_data = tokenizer.tokenize(english, layer=None)
+        target_train_data = tokenizer.tokenize(french, layer="decoder")
+       
+        self.val_data = train_data[:5000].detach().clone().cpu()
+        self.target_val_data = target_train_data[:5000].detach().clone().cpu()
+        self.test_data = train_data[5000:10000].detach().clone().cpu()
+        self.target_test_data = target_train_data[5000:10000].detach().clone().cpu()
+
+        self.train_data = train_data[10000:]
+        self.target_train_data = target_train_data[10000:]
+
+    def forward(self):
+        return self.train_data, self.target_train_data, self.val_data, self.target_val_data, self.test_data, self.target_test_data
