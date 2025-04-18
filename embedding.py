479c634e7816a06c5bf12b91a1ecf1bfec0629fb
Rohit Sali <155815075+Rohit2sali@users.noreply.github.com>
2025-04-18 14:09:11 +0530

    Update embedding.py

--- embedding.py
+++ embedding.py
@@ -1,22 +1,22 @@
+import math
 import torch
 import torch.nn as nn
-import math
 class word_embedding(nn.Module):
     def __init__(self, vocab_len, max_seq_len : int, d_model): 
         super(word_embedding, self).__init__()
         self.max_seq_len = max_seq_len
         self.d_model = d_model
         self.embedding = torch.nn.Embedding(vocab_len, d_model)
-        positional_embedding = torch.zeros(self.max_seq_len, self.d_model)
-        for pos in range(self.max_seq_len):
-            for i in range(0 , self.d_model, 2): 
-                positional_embedding[pos][i] = math.sin(pos / (10000 ** ((2* i) / self.d_model)))
-                positional_embedding[pos][i+1] = math.cos(pos / (10000 ** ((2*i+1) / self.d_model)))
-        self.positional_embedding = positional_embedding.reshape(1, self.max_seq_len, self.d_model)
-
+        pos = torch.arange(max_seq_len).unsqueeze(1)  # Shape: (seq_len, 1)
+        div_term = torch.exp(torch.arange(0, d_model, 2) * (-math.log(10000.0) / d_model))
+    
+        pe = torch.zeros(max_seq_len, d_model)
+        pe[:, 0::2] = torch.sin(pos * div_term)  
+        pe[:, 1::2] = torch.cos(pos * div_term)  
+        self.register_buffer('pe', pe)
+        
     def get_embedding(self, tokens):
-        tokens = tokens.long()
-        word_embedding = self.embedding(tokens)
-        positional_embedding = self.positional_embedding.to(word_embedding.device)
-        word_embedding = word_embedding + positional_embedding
+        tokens = tokens.long().to(self.embedding.weight.device)
+        word_embedding = self.embedding(tokens) * math.sqrt(self.d_model)
+        word_embedding = word_embedding + self.pe
         return word_embedding
